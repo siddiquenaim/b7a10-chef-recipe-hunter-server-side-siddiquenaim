cfdebaa3c532ec8132d8f5d9aba1429f93d6c150
siddiquenaim <naim.niter.tex06@gmail.com>
2023-05-02 13:33:48 +0600

    added cors

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 
+const cors = require("cors");
+app.use(cors());
+
 const chefs = require("./data/chefs.json");
 
 app.get("/", (req, res) => {
