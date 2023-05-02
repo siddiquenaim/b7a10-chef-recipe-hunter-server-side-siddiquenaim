d3f80aea483b8dd53b3f1db0fb91c72035ce259e
siddiquenaim <naim.niter.tex06@gmail.com>
2023-05-02 22:30:13 +0600

    completed recipe data and sent all data

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors");
 app.use(cors());
 
 const chefs = require("./data/chefs.json");
+const recipes = require("./data/recipes.json");
 
 app.get("/", (req, res) => {
   res.send("recipes are coming");
@@ -13,6 +14,19 @@ app.get("/", (req, res) => {
 
 app.get("/chefs", (req, res) => {
   res.send(chefs);
+});
+
+app.get("/chefs/:id", (req, res) => {
+  const id = req.params.id;
+  const chef = chefs.find((singleChef) => singleChef.chefId == id);
+  res.send(chef);
+});
+
+app.get("/recipes/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  const chefRecipes = recipes.filter((recipe) => recipe.chefId == id);
+  res.send(chefRecipes);
 });
 
 app.listen(port, () => {
